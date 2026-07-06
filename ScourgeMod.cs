858c7e2dfd2a427e4458d9e5ea1d30900a94e353
hugo.zhang <hugo.zhang98@qq.com>
2026-07-06 20:14:35 +0800

    Initial commit

---
+++ ScourgeMod.cs
@@ -0,0 +1,10 @@
+using Terraria.ModLoader;
+
+namespace ScourgeMod
+{
+    // Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
+    public class ScourgeMod : Mod
+    {
+
+    }
+}
